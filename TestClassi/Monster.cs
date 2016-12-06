def4a2cb935c4bc2f809de5866ba6d568c6bc37b
Elena <by-ele@hotmail.it>
2016-12-06 19:04:41 +0100

    Progetto Monster

--- TestClassi/Monster.cs
+++ TestClassi/Monster.cs
@@ -5,26 +5,59 @@ namespace TestClassi
 {
     class Monster
     {
-        public string name;
-        public int maxHp;
-        public int curHp;
+        private string _name;
+        public string name { get { return _name; } }
+
+
+        private int _maxHp;
+        public int maxHp
+        {
+            get
+            {
+                return _maxHp;
+            }
+        }
+
+
+        private int _curHp;
+        public int curHp
+        {
+            set
+            {
+                if (value < 0) value = 0;
+                else if (value > _maxHp) value = _maxHp;
+                _curHp = value;
+            }
+
+            get
+            {
+                return _curHp;
+            }
+        }
+
+
         public int damage;
         public int healFactor;
         
         public Monster(string name, int maxHp, int damage, int healFactor = 0)
         {
-            this.name = name;
-            this.maxHp = maxHp;
+            _name = name;
+
+            if (maxHp < 1) maxHp = 1;
+            _maxHp = maxHp;
+
             curHp = maxHp;
+
             this.damage = damage;
             this.healFactor = healFactor;
             describe();
         }
 
+
         public string describe()
         {
-            string output = "Questo è " + name + "\r\n";
-            output += "HP: " + curHp + "\r\n";
+            string output = "Questo è " + _name + "\r\n";
+            output += "HP: " + _curHp + "\r\n";
             output += "DAMAGE: " + damage + "\r\n";
 
             return output;
@@ -43,27 +76,27 @@ namespace TestClassi
                 return;
             }
 
-            if (curHp <= 0)
+            if (_curHp <= 0)
             {
                 Console.WriteLine("Non puoi curare nessuno da morto");
                 return;
             }
 
-            if (target.curHp <= 0)
+            if (target._curHp <= 0)
             {
-                Console.WriteLine(target.name + " è esausto e non puoi resuscitarlo con la cura.");
+                Console.WriteLine(target._name + " è esausto e non puoi resuscitarlo con la cura.");
                 return;
             }
 
-            target.curHp += healFactor;
+            target._curHp += healFactor;
 
-            if (target.curHp > target.maxHp)
+            if (target._curHp > target._maxHp)
             {
-                target.curHp = target.maxHp;
+                target._curHp = target._maxHp;
             }
 
-            Console.WriteLine(name + " usa cura su " + target.name);
-            Console.WriteLine(target.name + " è stato curato e ora ha " + target.curHp + "/" + target.maxHp + " HP");
+            Console.WriteLine(_name + " usa cura su " + target._name);
+            Console.WriteLine(target._name + " è stato curato e ora ha " + target._curHp + "/" + target._maxHp + " HP");
         }
 
         public void heal()
@@ -73,29 +106,29 @@ namespace TestClassi
 
         public void attack(Monster target)
         {
-            if (curHp <= 0)
+            if (_curHp <= 0)
             {
                 Console.WriteLine("Non puoi attaccare nessuno da morto");
                 return;
             }
 
-            if (target.curHp <= 0)
+            if (target._curHp <= 0)
             {
-                Console.WriteLine(target.name + " è già esausto, non infierire.");
+                Console.WriteLine(target._name + " è già esausto, non infierire.");
                 return;
             }
 
-            Console.WriteLine(name + " attacca " + target.name);
-            Console.WriteLine(name + " fa " + damage + " danni a " + target.name);
-            target.curHp -= damage;
+            Console.WriteLine(_name + " attacca " + target._name);
+            Console.WriteLine(_name + " fa " + damage + " danni a " + target._name);
+            target._curHp -= damage;
             
-            if (target.curHp <= 0 )
+            if (target._curHp <= 0 )
             {
-                target.curHp = 0;
-                Console.WriteLine(target.name + " è esausto.");
+                target._curHp = 0;
+                Console.WriteLine(target._name + " è esausto.");
             } else
             {
-                Console.WriteLine("a " + target.name + " rimangono " + target.curHp + " hp");
+                Console.WriteLine("a " + target._name + " rimangono " + target._curHp + " hp");
             }
         }
     }
